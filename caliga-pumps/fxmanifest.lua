dd64eb0416816af1caa25b0e3d7070cf6ada6d4c
Ap3x99 <74175693+Ap3x99@users.noreply.github.com>
2022-10-24 17:54:19 -0500

    added maps
    Added new maps.
    bra-blacksmith - Adds blacksmith workshop to Braithwaite manor
    caliga-pumps - Adds more water pumps to Caliga Hall
    emerald-hitch - Adds some hitching posts in convenient spots around Emerald Ranch
    realestate-office - Converts art gallery in Saint Denis to a real estate office & appartment
    rhodes-cleanup - Cleans up the back of the general store and surrounding area
    rhodes-doctors - Adds a few tents at the back of the Rhodes doctor's building allowing for med RP to take place in the absence of an interior
    rhodes-hangout - Adds a hangout spot at the gazebo behind the general store in Rhodes and cleans up the clutter
    shadybelle-tnt - Removes the TNT boxes & bundles inside the Shady Belle residence
    val-clothing - Removes the furniture inside the Valentine motel where the clothing store is (only really useful if your clothing store is where it is in my server)
    val-doctors - Revamps the doctors office in Valentine
    watchtower - Boards up the open windows of the old watch tower in the northern mountainous region of New Hanover, effectively making it a viable home.

---
+++ caliga-pumps/fxmanifest.lua
@@ -0,0 +1,9 @@
+fx_version 'adamant'
+game 'rdr3'
+rdr3_warning 'I acknowledge that this is a prerelease build of RedM, and I am aware my resources *will* become incompatible once RedM ships.'
+
+description 'Desperado Roleplay XMLs'
+
+dependency 'objectloader'
+file 'pumps.xml'
+objectloader_map 'pumps.xml'
